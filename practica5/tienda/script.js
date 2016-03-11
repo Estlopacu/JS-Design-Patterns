d1f598c135346738a75f7f16f2f372743bf4f012
Estlopacu <estlopacu@gmail.com>
2016-03-10 19:42:10 -0800

    Agregando comentarios

--- practica5/tienda/script.js
+++ practica5/tienda/script.js
@@ -9,17 +9,23 @@ function ropa(tipo, descripcion, nuBotones){
 };
 
 function guardarRopa(){
+	// obtenemos los valores de los campos
 	var descripcion = document.getElementById("descripcion").value;
 	var tipo = document.getElementById("tipo").value;
+	
+	// arreglo de objectos
 	var listado = [];
 
+	// se instancia un objecto
 	var ropa = new self.ropa(tipo,descripcion);
 
+	// se agrega al arreglo
 	listado.push(ropa);
 
 	for (var i = 0; i < listado.length; i++) {
 		self.listaObjectos += '<div class="row"><h4>'+listado[0].descripcion+'</h4><p>Tipo: '+listado[0].tipo+', numero de botones: '+listado[0].nuBotones+'</p></div><hr/>'
 	}
 
+	// se desplega en html
 	document.getElementById("resultado").innerHTML = self.listaObjectos;
 }
